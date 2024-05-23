5ed3045ca38e353cf5458322292745c9282c3c79
David Li <dtli@lcs.on.ca>
2024-05-23 17:42:11 -0400

    Adding list and ui of display single item

--- MinimalistItem/View/ItemListView.swift
+++ MinimalistItem/View/ItemListView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct ItemListView: View {
+    //MARK: Computing property
     var body: some View {
         NavigationView{
-            
+            List(testItems){currentdisplay in
+                HStack{
+                    
+                    VStack{
+                        Text(currentdisplay.name)
+                            .bold()
+        
+                        Text(currentdisplay.type)
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
         }.navigationBarTitle("Items", displayMode: .large)
     }
+    
+    //MARK: Function
 }
 
 #Preview {
