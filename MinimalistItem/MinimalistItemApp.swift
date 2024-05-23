5915ad10a42765b941f3e0c9a523c98756985b36
David Li <dtli@lcs.on.ca>
2024-05-23 17:26:36 -0400

    Initial Commit

---
+++ MinimalistItem/MinimalistItemApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalistItemApp.swift
+//  MinimalistItem
+//
+//  Created by David Li on 2024-05-23.
+//
+
+import SwiftUI
+
+@main
+struct MinimalistItemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
