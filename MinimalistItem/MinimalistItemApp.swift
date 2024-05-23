6e20ae889244bfcee5d84e7405bb96ea04b39b91
David Li <dtli@lcs.on.ca>
2024-05-23 17:32:02 -0400

    Add ItemView and navigation title of it

--- MinimalistItem/MinimalistItemApp.swift
+++ MinimalistItem/MinimalistItemApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MinimalistItemApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ItemListView()
         }
     }
 }
---
+++ MinimalistItem/View/ItemListView.swift
@@ -0,0 +1,22 @@
+//
+//  ItemListView.swift
+//  MinimalistItem
+//
+//  Created by David Li on 2024-05-23.
+//
+
+import SwiftUI
+
+struct ItemListView: View {
+    var body: some View {
+        NavigationView{
+            
+        }.navigationBarTitle("Items", displayMode: .large)
+    }
+}
+
+#Preview {
+    NavigationView{
+        ItemListView()
+    }
+}
