d0dca0de77ddf2b9c4d6b8e99a7e7f4d60691f9a
David Li <dtli@lcs.on.ca>
2024-05-31 19:49:04 -0400

    Adding the rest of the CRUD function in the database helper

--- MinimalistItem/Helpers/DatabaseHelper.swift
+++ MinimalistItem/Helpers/DatabaseHelper.swift
@@ -29,7 +29,36 @@ class DatabaseHelper {
             return nil
         }
     }
+    // Create
+        func createItem(item: Item) -> Bool {
+            let insertStatementString = "INSERT INTO Item (name, description, importance, type) VALUES (?, ?, ?, ?);"
+            var insertStatement: OpaquePointer? = nil
+            
+            if sqlite3_prepare_v2(db, insertStatementString, -1, &insertStatement, nil) == SQLITE_OK {
+                sqlite3_bind_text(insertStatement, 1, (item.name as NSString).utf8String, -1, nil)
+                if let description = item.description {
+                    sqlite3_bind_text(insertStatement, 2, (description as NSString).utf8String, -1, nil)
+                } else {
+                    sqlite3_bind_null(insertStatement, 2)
+                }
+                sqlite3_bind_int(insertStatement, 3, Int32(item.importance))
+                sqlite3_bind_text(insertStatement, 4, (item.type as NSString).utf8String, -1, nil)
+                
+                if sqlite3_step(insertStatement) == SQLITE_DONE {
+                    print("Successfully inserted row.")
+                    sqlite3_finalize(insertStatement)
+                    return true
+                } else {
+                    print("Could not insert row.")
+                }
+            } else {
+                print("INSERT statement could not be prepared.")
+            }
+            sqlite3_finalize(insertStatement)
+            return false
+        }
     
+    //read
     func readUsers() -> [Item] {
         var items: [Item] = []
         let queryString = "SELECT * FROM Item"
@@ -48,4 +77,56 @@ class DatabaseHelper {
         sqlite3_finalize(queryStatement)
         return items
     }
+    
+    //update
+        func updateItem(item: Item) -> Bool {
+            let updateStatementString = "UPDATE Item SET name = ?, description = ?, importance = ?, type = ? WHERE id = ?;"
+            var updateStatement: OpaquePointer? = nil
+            
+            if sqlite3_prepare_v2(db, updateStatementString, -1, &updateStatement, nil) == SQLITE_OK {
+                sqlite3_bind_text(updateStatement, 1, (item.name as NSString).utf8String, -1, nil)
+                if let description = item.description {
+                    sqlite3_bind_text(updateStatement, 2, (description as NSString).utf8String, -1, nil)
+                } else {
+                    sqlite3_bind_null(updateStatement, 2)
+                }
+                sqlite3_bind_int(updateStatement, 3, Int32(item.importance))
+                sqlite3_bind_text(updateStatement, 4, (item.type as NSString).utf8String, -1, nil)
+                sqlite3_bind_int(updateStatement, 5, Int32(item.id))
+                
+                if sqlite3_step(updateStatement) == SQLITE_DONE {
+                    print("Successfully updated row.")
+                    sqlite3_finalize(updateStatement)
+                    return true
+                } else {
+                    print("Could not update row.")
+                }
+            } else {
+                print("UPDATE statement could not be prepared.")
+            }
+            sqlite3_finalize(updateStatement)
+            return false
+        }
+    
+    //delete
+        func deleteItem(by id: Int) -> Bool {
+            let deleteStatementString = "DELETE FROM Item WHERE id = ?;"
+            var deleteStatement: OpaquePointer? = nil
+            
+            if sqlite3_prepare_v2(db, deleteStatementString, -1, &deleteStatement, nil) == SQLITE_OK {
+                sqlite3_bind_int(deleteStatement, 1, Int32(id))
+                
+                if sqlite3_step(deleteStatement) == SQLITE_DONE {
+                    print("Successfully deleted row.")
+                    sqlite3_finalize(deleteStatement)
+                    return true
+                } else {
+                    print("Could not delete row.")
+                }
+            } else {
+                print("DELETE statement could not be prepared.")
+            }
+            sqlite3_finalize(deleteStatement)
+            return false
+        }
 }
