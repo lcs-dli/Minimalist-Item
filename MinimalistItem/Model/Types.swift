29ab28c7292b605f1119321db4241f93eb6da13f
David Li <dtli@lcs.on.ca>
2024-05-31 20:08:27 -0400

    Adding type datatype to store different type of items

---
+++ MinimalistItem/Model/Types.swift
@@ -0,0 +1,14 @@
+//
+//  Types.swift
+//  MinimalistItem
+//
+//  Created by David Li on 2024-05-31.
+//
+
+import Foundation
+
+struct Types: Identifiable{
+    var id: Int?
+    var typeName: String
+    var itemCount: Int
+}
